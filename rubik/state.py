772493d554543c39b16eec6caf76febcfad6f7b3
Wee Hean Ng <heanconcerto@gmail.com>
2026-09-24 08:16:40 +0800

    Bug removal: type hint backward compatibility

--- rubik/state.py
+++ rubik/state.py
@@ -4,7 +4,7 @@ from typing import Protocol
 
 
 class State(Protocol):
-    def successors(self) -> list[State]:
+    def successors(self) -> list["State"]:
         """Return a list of successors from the current state.
 
         The list should exclude the predecessor of the current state.
